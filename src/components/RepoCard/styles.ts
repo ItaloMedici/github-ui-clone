e427b0c0ec3dbe8711ffb391ef0be25723625d85
ItaloMedici <italomedice969@gmail.com>
2022-01-16 01:14:10 -0300

    Top Header card style

--- src/components/RepoCard/styles.ts
+++ src/components/RepoCard/styles.ts
@@ -1,14 +1,43 @@
 import styled, { css } from 'styled-components';
 
-import { RiBookmarkLine, RiStarLine } from 'react-icons/ri';
+import { RiBookMarkLine, RiStarLine } from 'react-icons/ri';
 import { AiOutlineFork } from 'react-icons/ai';
 
 export const Container = styled.div`
-  
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  padding: 16px;
+  border: 1px solid var(--border);
+  border-radius: 6px;
 `;
 
 export const Top = styled.div`
+  > header {
+    display: flex;
+    align-items: center;
 
+    > a {
+      margin-left: 8px;
+      font-size: 14px;
+      font-weight: 600;
+      color: var(--link);
+
+      text-decoration: none;
+
+      &:focus, &:hover {
+        text-decoration: undeline;
+      }
+    }
+  }
+
+  > p {
+    margin: 8px 0 16px;
+    font-size: 12px;
+    color: var(--gray);
+    letter-spacing: .1px;
+  }
 `;
 
 const iconsCSS = css`
@@ -18,7 +47,7 @@ const iconsCSS = css`
   flex-shrink: 0;
 `;
 
-export const RepoIcon = styled(RiBookmarkLine)`${iconsCSS}`;
+export const RepoIcon = styled(RiBookMarkLine)`${iconsCSS}`;
 
 export const StarIcon = styled(RiStarLine)`${iconsCSS}`;
 
