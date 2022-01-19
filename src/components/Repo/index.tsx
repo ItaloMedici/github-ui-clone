707e1ba2f0b93dcc6658c30ed5a754beb9b06644
ItaloMedici <italomedice969@gmail.com>
2022-01-18 22:23:13 -0300

    styling repo page

--- src/components/Repo/index.tsx
+++ src/components/Repo/index.tsx
@@ -1,9 +1,55 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Breacdcrumb,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GitHubIcon
+} from './styles';
 
 const Repo: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Breacdcrumb>
+        <RepoIcon />
+
+        <Link className={"username"} to={"/ItaloMedici"}>
+          ItaloMedici
+        </Link>
+
+        <span>/</span>
+
+        <Link className={"reponame"} to={"/ItaloMedici/exercicios-C.Compt-UIT"}>
+          youtube-content
+        </Link>
+      </Breacdcrumb>
+
+      <p>Contains all exercices of my course</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>0</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={"https://github.com/ItaloMedici/Exercicios-C.Compt-UIT"}>
+        <GitHubIcon />
+        <span>View on GitHub</span>
+      </LinkButton>
+    </Container>
+  );
 }
 
 export default Repo;
--- src/components/Repo/styles.ts
+++ src/components/Repo/styles.ts
@@ -1,5 +1,101 @@
-import styled from 'styled-components';
+import { AiOutlineFork } from 'react-icons/ai';
+import { FaGithub } from 'react-icons/fa';
+import { RiBookMarkLine, RiStarLine } from 'react-icons/ri';
+import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
-  
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+
+  > p {
+    font-size: 16px;
+  }
 `;
+
+export const Breacdcrumb = styled.div`
+  margin-bottom: 16px;
+
+  display: flex;
+  align-items: center;
+  flex-wrap: nowrap;
+  white-space: nowrap;
+
+  font-size: 18px;
+
+  > a {
+    color: var(--link);
+    text-decoration> none;
+
+    &:hover, &:focus {
+      text-decoration: underline;
+    }
+
+    &.username {
+      margin-left: 8px;
+    }
+
+    &.reponame {
+      font-weight: 600;
+    }
+  }
+
+  > span {
+    padding: 0 5px;
+  }
+`;
+
+export const Stats = styled.div`
+  margin-top: 16px;
+  display: flex;
+  align-items: center;
+
+  > li {
+    display: flex;
+    align-items: center;
+    margin-right: 9px;
+
+    > * {
+      margin-right: 7px;
+      color: var(--gray);
+    }
+  }
+`;
+
+export const LinkButton = styled.a`
+  display: flex;
+  align-items: center;
+
+  margin-top: 24px;
+  background: var(--gray-dark);
+  padding: 12px 17px;
+  border-radius: 24px;
+  width: max-content;
+
+  text-decoration: none;
+
+  > span {
+    color: var(--primary);
+  }
+
+  > svg {
+    fill: var(--primary);
+    margin-right: 10px;
+  }
+`;
+
+const icon = css`
+  width: 16px;
+  height: 16px;
+  fill: var(--icon);
+  flex-shrink: 0;
+
+`;
+
+export const RepoIcon = styled(RiBookMarkLine)`${icon}`;
+
+export const ForkIcon = styled(AiOutlineFork)`${icon}`;
+
+export const StarIcon = styled(RiStarLine)`${icon}`;
+
+export const GitHubIcon = styled(FaGithub)`${icon}`;
